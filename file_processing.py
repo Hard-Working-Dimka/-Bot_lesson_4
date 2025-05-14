c182da8690dfe564c370db20aa5aa9f56bca7f63
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-05-14 20:10:14 +0300

    init

---
+++ file_processing.py
@@ -0,0 +1,18 @@
+with open("questions/1vs1200.txt", "r", encoding='KOI8-R') as file:
+    questions = file.read()
+
+question_blocks = questions.split('\n\n\n')
+questions = {}
+
+
+for question_block in question_blocks:
+    question_block = question_block.strip().split('\n\n')
+    for question in question_block:
+        if question.startswith('Вопрос'):
+            question_name = question.split('\n', maxsplit=1)[1].replace('\n', ' ').strip()
+        if question.startswith('Ответ'):
+            question_answer = question.split('\n', maxsplit=1)[1].replace('\n', ' ').strip()
+    questions[question_name] = question_answer
+    break
+
+
